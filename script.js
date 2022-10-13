a4935d240528726dead850f3a1ca1c6b5b2c3b8e
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-10-13 10:18:19 -0400

    removed api key

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //Geocoding API call 
-var geoCoding = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=d97fe2285b7bc123de0716fce9e4ac7a"
+var geoCoding = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={api key}"
 
 //API call for currernt weather 
-var currentRequestUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=d97fe2285b7bc123de0716fce9e4ac7a"
+var currentRequestUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api key}"
 
 //API call for 5 day forcast 
-var fiveDayRequestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=d97fe2285b7bc123de0716fce9e4ac7a";
+var fiveDayRequestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api key}";
 
 $.ajax({
     url: currentRequestUrl,
