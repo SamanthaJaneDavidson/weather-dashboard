cf98583719263bb464fc78c1fdf55f697d6fb6c7
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-10-16 14:55:40 -0400

    started adding city link storage

--- script.js
+++ script.js
@@ -2,43 +2,76 @@ var currentWeatherReport = document.querySelector(`#current-weather-report`);
 var fiveDayreport = document.querySelector(`#five-day-report`);
 var city = document.querySelector("#city");
 var searchButton = document.querySelector("#search")
+var citySearches = document.querySelector("#city-links")
 
 //Search for city
 var searchCity = function (event) {
     event.preventdefault();
 
-    city.value();
+    var city = city.value.trim();
 
     if (city) {
         getLongLat(city);
 
-        currentWeatherReport.textContent = ` `;
-
     } else {
-        alert(`Please enter a valid city and.`);
+        alert(`Please enter a valid city`);
     }
 };
 
-//Event listener for search button to kick city to long/lat conversion 
-searchButton.addEventListener(`click`, getLongLat);
+
+//Save city search links 
+var cities = JSON.parse(localStorage.getItem("cities")) || [];
+
+function storeCitySearches(){
+    var storedCities = {
+        city: city.value.trim()
+    };
+    cities.push(storedCities)
+    localStorage.setItem("cities", JSON.stringify(cities))
+}
+
+
+//Display city search links 
+function renderCitySearches () {
+    var savedSearches = JSON.parse(localStorage.getItem("storedCities"));
+
+    if(savedSearches !== null) {
+        document.getElementById("city-links").innerHTML = savedSearches;
+    } else {
+        return;
+    }
+}
+
+// Event listener for search button to save scores and go to geo conversion 
+searchButton.addEventListener(`click`, function (event) {
+    event.preventDefault();
+    storeCitySearches();
+    renderCitySearches();
+    getLongLat();
+});
+
 
 //City to long/lat geocoding converstion 
 function getLongLat(city){
-    var geoCodingUrl = `https://api.openweathermap.org/data/2.5/weather?q=` + city + `&appid=d97fe2285b7bc123de0716fce9e4ac7a`;
+    var geoCodingUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d97fe2285b7bc123de0716fce9e4ac7a";
 
     fetch(geoCodingUrl)
         .then(function (response) {
             response.json();
         })
         .then(function (data) {
-            console.log(data)
+            console.log(data);
         })
 
+        //not sure how to define lon and lat here
+
+       getCurrentWeather();
 }
 
-//Get current weather forcast
-var getCurrentWeather = function (currentWeather) {
-    var currentRequestUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=d97fe2285b7bc123de0716fce9e4ac7a";
+
+//Get current weather forecast
+var getCurrentWeather = function () {
+    var currentRequestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +" &appid=d97fe2285b7bc123de0716fce9e4ac7a";
 
     fetch(currentRequestUrl)
         .then(function (response) {
@@ -46,40 +79,37 @@ var getCurrentWeather = function (currentWeather) {
     }) 
         .then(function (data) {
             console.log(data);
-            for(var i = 0; i < data.length; i++){
+        })
+    };
+    
+//Display current weather forecast
+var displayCurrentForcast = function (city) {
+    for(var i = 0; i < data.length; i++) {
 
-                //create
-                var cityName = document.createElement(`h2`);
-                var currentDate = document.createElement(`h2`);
-                var weatherIcon = document.createElement(`div`);
-                var currentTemp = document.createElement(`p`);
-                var currentWind = document.createElement(`p`);
-                var currentHumidity  = document.createElement(`p`);
+        var cityName = document.createElement(`h2`);
+        var currentDate = document.createElement(`h2`);
+        var weatherIcon = document.createElement(`div`);
+        var currentTemp = document.createElement(`p`);
+        var currentWind = document.createElement(`p`);
+        var currentHumidity  = document.createElement(`p`);
 
-                //modify
-                cityName.textContent = location //?
-                currentDate = //? 
-                weatherIcon = data[i].weather.icon;
-                currentTemp = data[i].main.temp;
-                currentWind = data[i].wind.speed;
-                currentHumidity = data[i].main.humidity;
+        cityName.textContent = city
+        currentDate = moment().format (`L`);
+        weatherIcon = data[i].weather.icon;
+        currentTemp = data[i].main.temp;
+        currentWind = data[i].wind.speed;
+        currentHumidity = data[i].main.humidity;
 
-                //append
-                currentWeatherReport.append(cityName);
-                currentWeatherReport.append(currentDate);
-                currentWeatherReport.append(weatherIcon);
-                currentWeatherReport.append(currentTemp);
-                currentWeatherReport.append(currentWind);
-                currentWeatherReport.append(currentHumidity);
-            }
+        currentWeatherReport.append(cityName);
+        currentWeatherReport.append(currentDate);
+        currentWeatherReport.append(weatherIcon);
+        currentWeatherReport.append(currentTemp);
+        currentWeatherReport.append(currentWind);
+        currentWeatherReport.append(currentHumidity);
+        }
+    };
 
-
-
-//Display current weather forcast 
-var displayCurrentWeather = function (weather, city) {
-
-}
-
+//Get 5 day forecast 
 
 
 // //API call for currernt weather 
