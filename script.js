0e84d6ae382f231338d2c863ca196c375a9f32ca
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-10-18 21:49:59 -0400

    Fixed search history links; updated date format

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ var city = ``;
 //Search for city
 var searchCity = function (event) {
 
-    city = cityInput.value.trim();
+    if (event.target.value !== "search"){
+        city = event.target.value;
+    }
+    else {
+        city = cityInput.value.trim();
+    }
+
     console.log(city)
 
     if (city) {
@@ -18,36 +24,44 @@ var searchCity = function (event) {
             city: city
         };
 
-        cities.push(storedCities)
+        var existing = false
+        for(var i = 0; i < cities.length; i++ ){
+            if (cities[i].city == storedCities.city){
+                existing = true 
+            }
+        }
+
+        if(existing == false){
+            cities.push(storedCities)
+        }
+
         localStorage.setItem("cities", JSON.stringify(cities))
     } else {
         alert(`Please enter a valid city`);
     }
+    renderCitySearches()
 };
 
 // //Display city search links 
 var cities = JSON.parse(localStorage.getItem("cities")) || [];
 
 function renderCitySearches () {
-    var savedSearches = JSON.parse(localStorage.getItem("cities"));
-    console.log(savedSearches);
 
-    if(savedSearches !== null) {
-        document.getElementById("city-search-links").innerHTML = savedSearches;
+    if(cities !== null) {
+        document.getElementById("city-search-links").innerHTML = cities;
     } else {
         return;
     }
 
     citySearchesEl.innerHTML = ``;
 
-    for(var i = 0; i < 10; i++) {
-        console.log(savedSearches[i]);
-        var citySearchEl = document.createElement("a");
-        // citySearchEl.setAttribute("href", ) 
-        citySearchEl.textContent = savedSearches[i].city;
+    for(var i = 0; i < cities.length; i++) {
+        console.log(cities[i]);
+        var citySearchEl = document.createElement("button");
+        citySearchEl.textContent = cities[i].city;
+        citySearchEl.value = cities[i].city;
         citySearchesEl.append(citySearchEl);
-
-        //not sure how to make these into links to search again and also take out dups
+        citySearchEl.addEventListener('click', searchCity);
     }
 }
     renderCitySearches();
@@ -55,7 +69,7 @@ function renderCitySearches () {
 // Event listener for search button to save scores and go to geo conversion 
 searchButton.addEventListener(`click`, function (event) {
     event.preventDefault();
-    searchCity();
+    searchCity(event);
 });
 
 
@@ -86,13 +100,15 @@ var getCurrentWeather = function (lat, lon) {
         .then(function (data) {
             console.log(data);
             displayCurrentWeather(data);
-            displayForecast(data) //change to full 5 day if possible 
+            displayForecast(data)
         })
     };
     
 //Display current weather forecast
 
 var displayCurrentWeather = function (data) {
+
+    currentWeatherReport.innerHTML = ``; 
 
         var cityName = document.createElement(`h2`);
         var currentDate = document.createElement(`h4`);
@@ -101,8 +117,11 @@ var displayCurrentWeather = function (data) {
         var currentWind = document.createElement(`li`);
         var currentHumidity  = document.createElement(`li`);
 
+        var date = new Date(data.current.dt * 1000);
+        console.log(date);
+
         cityName.textContent = city;
-        currentDate.textContent = data.current.dt; //will need to do date conversion
+        currentDate.textContent = date; 
         weatherIcon.setAttribute("src","https://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png")
         currentTemp.textContent = "Temperature: " + data.current.temp;
         currentWind.textContent = "Wind Speed: " + data.current.wind_speed;
@@ -115,33 +134,39 @@ var displayCurrentWeather = function (data) {
         currentWeatherReport.append(currentWind);
         currentWeatherReport.append(currentHumidity);
 
-        // var dateConversion = new Date(currentDate).toLocaleDateString("en-US");
-        // console.log(dateConversion);
-
     };
 
 
 //Get 5 day forecast 
 var displayForecast = function (data) {
 
-    for (var i = 0; i < 5; i++) {
+    forecastWeatherReport.innerHTML = ``;
 
+    for (var i = 0; i < 5; i++) {
+    
+    var div = document.createElement('div');
+    div.classList.add('card');
     var forecastDate = document.createElement(`h4`);
     var forecastIcon = document.createElement(`img`);
     var forecastTemp = document.createElement(`li`);
     var forecastWind = document.createElement(`li`);
     var forecastHumidity  = document.createElement(`li`);
 
-    currentDate = data.daily[i].dt; //need to do a date conversion 
+    var date = new Date(data.daily[i].dt * 1000);
+        console.log(date);
+
+    forecastDate = date; //need to do a date conversion 
     forecastIcon.setAttribute("src","https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png")
     forecastTemp.textContent = "Temperature: " + data.daily[i].temp.day;
     forecastWind.textContent = "Wind Speed: " + data.daily[i].wind_speed;
     forecastHumidity.textContent = "Humidity: " + data.daily[i].humidity;
 
-    forecastWeatherReport.append(forecastDate);
-    forecastWeatherReport.append(forecastIcon);
-    forecastWeatherReport.append(forecastTemp);
-    forecastWeatherReport.append(forecastWind);
-    forecastWeatherReport.append(forecastHumidity);
+    div.append(forecastDate);
+    div.append(forecastIcon);
+    div.append(forecastTemp);
+    div.append(forecastWind);
+    div.append(forecastHumidity);
+
+    forecastWeatherReport.append(div);
 };
 }
